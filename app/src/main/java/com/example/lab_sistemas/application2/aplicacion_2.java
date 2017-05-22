c4f57e119c47f13d6a471015d7a0f90b39b25d2a
Richard <you@example.com>
2017-05-22 18:50:11 -0400

    nuevo

---
+++ app/src/main/java/com/example/lab_sistemas/application2/aplicacion_2.java
@@ -0,0 +1,13 @@
+package com.example.lab_sistemas.application2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class aplicacion_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aplicacion_2);
+    }
+}
